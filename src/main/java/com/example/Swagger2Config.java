2d04d7702e98f3993a322b1481df7968df342fb7
qijunbo <junboqi@foxmail.com>
2018-07-21 14:23:21 +0800

    add swagger2 as default.  and add index page.

---
+++ src/main/java/com/example/Swagger2Config.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sunway Cloud RESTful APIs")
+                .description("Sunway Cloud RESTful APIs")
+                .termsOfServiceUrl("http://cloud.sunwayworld.com/")
+                .contact(new Contact("Sunwayworld", "http://cloud.sunwayworld.com", "kefu@sunwayworld.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
