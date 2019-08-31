27e81933d8aeaba7e2c683cafcff73207279cf5d
junboqi <junboqi@foxmail.com>
2019-08-31 10:05:18 +0800

    事情只要在发展，那它就不断完善；而发展直到死亡的那一刻才结束。

--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
+#!/bin/sh
+chmod 770 gradlew
 ./gradlew clean build --daemon
+#./mvnw clean package
+workfolder=/opt/apps
+mkdir -p ${workfolder}
+app=swaggerDemo
+version=1.0
+
+mkdir -p ${workfolder}/${app}
+sudo systemctl stop ${app}
+cp -vpf build/libs/${app}.jar  ${workfolder}/${app}/${app}.jar
+#cp -vpf target/${app}.jar  ${workfolder}/${app}/${app}.jar
+chmod 755 ${workfolder}/${app}/${app}.jar
+sudo ln -sf  ${workfolder}/${app}/${app}.jar  /etc/init.d/${app}
+sudo systemctl daemon-reload
+#sudo chkconfig ${app} on
+sudo systemctl enable ${app}
+sudo systemctl start ${app}
