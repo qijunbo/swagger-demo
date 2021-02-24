258b5dc71314f910b3de773fa5b09878f8551d69
齐俊波 <qijunbo@users.noreply.github.com>
2021-02-24 13:48:36 +0800

    Update build.bat
    太怀恋以前自由的时刻.

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 echo timestamp=%date:~0,10% %time:~0,8% >src\main\resources\version.properties
 echo version=1.2 >>src\main\resources\version.properties
 gradlew clean build
+read  -n 1  -p "press any key to continue..." 
